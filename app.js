5f8666ea05dd6808ee3317c73fb91e7d5531e531
Luis Uranga <luisuranga.urangaruiz@altran.com>
2019-07-05 20:43:35 +0200

    cambio config

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const appConfig = require("./config/app-env");
-
-require("./routes")(app, appConfig);
-
-app.listen(appConfig.port,
-  () => console.log(`App listening on port ${appConfig.port}`)
-);
